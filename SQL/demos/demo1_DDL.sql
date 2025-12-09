35c75440003348653c5da22b942c4a8dace85313
Loick <loick@utopios.solutions>
2025-12-09 12:14:10 +0100

    Modification de la demo1

--- SQL/demos/demo1_DDL.sql
+++ SQL/demos/demo1_DDL.sql
@@ -1,52 +1,61 @@
--- ------ INSERTION ----------
+-- DDL : DATA DEFINITION LANGAGE
+-- Pour faire un commentaire, on utilise deux "-" suivi d'un espace 
 
--- Pour insérer une valeur dabs une table, nous utilisons la commande INSERT INTO
--- Si aucune parathèse n'est mise nous devons renseigner la totalité des champs
-INSERT INTO services (libelle) 
--- Le mot-clé VALUES nous permet d'insérer les valeurs précisé dans le INSERT INTO
-VALUES ('Informatique'); 
+-- Création d'une base de donnée
+-- La création d'une base de donnée en MySQL implique de créer 
+-- une seconde connexion pour y accéder.
+-- Pour s'assurer de l'existence (ou non) d'un élément, nous pouvons ajouter IF (NOT) EXISTS
+CREATE DATABASE IF NOT EXISTS demo1; 
 
-ALTER TABLE services
-ADD COLUMN date_creation DATE;
+-- Suppression d'une base de donnée.
+DROP DATABASE IF EXISTS demo1;
 
--- Nous pouvons insérer plusieurs valeurs d'un coup en séparant les enregistrements par une ','
-INSERT INTO services (libelle, date_creation)
-VALUES
-	('RH', '2025-12-09'),
-    ('Comptabilite', '2025-12-09') ;
+-- Pour travailler dans la base donnée 'demo1', il est nécessaire de le spécifié à MySQL
+USE demo1; 
 
--- Format timestamp (date-heure) : AAAA-MM-DD HH:mm:SS
+-- ----------- CREATE TABLE -------------------
 
--- -------- MISE A JOUR ------------------
+-- Pour créer une table, on utilise l'instruction CREATE TABLE
+-- On peut vérifier que la table existe avant de la créer.
+CREATE TABLE IF NOT EXISTS Services (
+	service_id INT AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(255)
+);
 
--- Cette commande permet de retirer le safe mode pour les mise à jour et permettre les modifications sur l'ensemble de la table.
-SET SQL_SAFE_UPDATES = 0;
+-- Suppression d'une table si elle existe.
+DROP TABLE IF EXISTS Services; 
 
--- Pour mettre à jour une ou plusieurs dans une table, nous utilisons le mot-clé UPDATE suivie de SET.
-UPDATE services
-SET date_creation = '2025-12-09'
-WHERE service_id = 1;
+-- Pour modifier une table, nous utilisont le mot-clé ALTER TABLE
+-- Pour la mofication, nous pouvons ajouter des colonnes.
+ALTER TABLE Services
+ADD COLUMN date_creation DATE; 
 
--- Pour mettre à jour plusieurs attribut, il suffit de les préciser dans le SET en les séparant par une virgule.
-UPDATE services
-SET libelle = 'Ressources Humaines', date_creation = '2025-12-09'
-WHERE service_id = 2;
+-- Pour modifier le type d'une colonne.
+ALTER TABLE Services
+MODIFY date_creation DATETIME; 
 
--- ------ SUPPRESSION --------------
+-- Nous pouvons également en supprimer. 
+ALTER TABLE Services
+DROP COLUMN date_creation;
 
--- Pour supprimer un élément par son id
-DELETE FROM services
-WHERE service_id = 3;
+-- Et nous pouvons aussi renommé nos différents tables.
+ALTER TABLE Services
+RENAME TO services_plus; 
 
--- Attention en l'absence de WHERE, le DELETE va supprimer l'ensemble des enregistrements de la table.
-DELETE FROM services;
-
--- Le TRUNCATE effectue également une suppression intégrale de la table de façon plus performante MAIS c'est irréversible.
-TRUNCATE services; 
-
-
-
-
-
-
-
+-- Créer une table avec des contraintes
+CREATE TABLE IF NOT EXISTS salarie (
+	-- Primary Key permet d'ajouter une contrainte de type clé primaire (UNIQUE + NOT NULL)
+	salarie_id INT AUTO_INCREMENT PRIMARY KEY,
+    -- Not Null est une contrainte empêchant l'absence de valeur
+    prenom VARCHAR(255) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    -- la contrainte CHECK permet d'ajouter un mécanisme de vérification
+    age INT NOT NULL CHECK (age >= 18),
+    -- la contriante DEFAULT permet de définir une valeur par défaut en cas d'absence de valeur renseigné.
+    salaire DECIMAL DEFAULT 1500.0,
+    service_id INT NOT NULL, 
+    -- Il est possible d'ajouter des contraintes nommée avec le mot-clé CONSTRAINT
+    -- Pour les clé étrangères nous devons renseigner le champs de la table salarie qui sera notre clé étrangère
+    -- Puis dans REFERENCES, nous devons spécifer la table et la clé primaire qui serait en relation avec notre table actuel.
+    CONSTRAINT fk_service_id FOREIGN KEY (service_id) REFERENCES Services(service_id)
+); 
